6ddc25c01d4bd99393cf03df9e1dbe30268732df
gnmathew <genemathewdorado@gmail.com>
2024-04-23 22:43:40 +0800

    add devise user routes and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 end
